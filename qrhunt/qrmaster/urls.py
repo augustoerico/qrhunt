ace820c45849860b92550f449317b30ec518e3a7
Erico Silva <augusto.ericosilva@gmail.com>
2015-09-25 00:54:50 -0300

    Implemented login and logout

--- qrhunt/qrmaster/urls.py
+++ qrhunt/qrmaster/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^login/$', views.do_login, name='login'),
+    url(r'^logout/$', views.do_logout, name='logout'),
 ]
--- qrhunt/qrmaster/views.py
+++ qrhunt/qrmaster/views.py
@@ -1,6 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
+@login_required(login_url='qrmaster:login')
 def index(request):
-    
-	return render(request, 'qrmaster/index.html', {})
+	return render(request, 'qrmaster/index.html')    
 
+def do_login(request):
+    
+    if request.method == 'GET':
+        return render(request, 'qrmaster/login.html')
+    
+    elif request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+           if user.is_active:
+                messages.add_message(request, messages.SUCCESS, 'Login successful.')
+                login(request, user)
+                return redirect('qrmaster:index')
+
+        messages.add_message(request, messages.ERROR, 'Failed at login.')
+        return redirect('qrmaster:login')
+
+def do_logout(request):
+    logout(request)
+    return redirect('qrmaster:login')
