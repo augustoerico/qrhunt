f9ea62e11b06692d77f21e6347350c4d64f844cb
Erico Silva <augusto.ericosilva@gmail.com>
2015-10-03 10:49:15 -0300

    Fix primary key generation in Hint

--- qrhunt/qrmaster/models.py
+++ qrhunt/qrmaster/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from base64 import b32encode
+from hashlib import sha1
+from random import random
+
+def pk_gen():
+    return b32encode(sha1(str(random())).digest()).lower()[:6]
+
 class Quest(models.Model):
-    master = models.OneToOneField(User, on_delete=models.CASCADE)
+    master = models.ForeignKey(User)
+    
     name = models.CharField(max_length=30)
     
 class Hint(models.Model):
+    id = models.CharField(max_length=6, primary_key=True, default=pk_gen)
+    quest = models.ForeignKey(Quest)
+    
     title = models.CharField(max_length=30)
     message = models.CharField(max_length=200)
-    
-    quest = models.ForeignKey(Quest)
